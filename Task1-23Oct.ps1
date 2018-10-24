e18c6c5e9758b6fd56d44129635ce2a095a97dde
Sumitd13 <44285826+Sumitd13@users.noreply.github.com>
2018-10-24 11:09:08 +0530

    Task1 23Oct18
    Task1 23Oct18

---
+++ Task1-23Oct.ps1
@@ -0,0 +1,11 @@
+﻿<#Identify a cmdlet that produces a random number#>
+Get-Random
+<#Identify a cmdlet that displays the current dat and time#>
+Get-Date
+<#What type of object the cmdlet from task 2 Produce?#><#Command used : Get-Date | GM #>
+System.DateTime
+
+<#Identify the cmdlet that displays information about installed hotfixes on windows#>
+Get-HotFix
+<#Display a list of 50 newest entries from security log#>
+Get-EventLog -LogName Security -Newest 50
